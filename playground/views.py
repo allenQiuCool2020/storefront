b0caa62836434c6e3f780071a5bb56bc58eface7
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2024-04-10 22:03:46 +0800

    commit before admin learning
    commit before admin learning

--- playground/views.py
+++ playground/views.py
@@ -7,9 +7,25 @@ from django.db.models.aggregates import Max, Min, Count, Avg
 # Create your views here.
 
 def hello_world(request):
+    # it is test
     # queryset = Product.objects.filter(collection__id=3).order_by('unit_price')
     # queryset = OrderItem.objects.values('product_id').distinct()
     # queryset = Product.objects.prefetch_related('promotions').all()
+
+        # queryset = Customer.objects.annotate(
+    #     full_name = Concat(F('first_name'),Value(' ') ,F('last_name'))
+    # )
+
+    # grouping data
+
+    # queryset = Customer.objects.annotate(
+    #     orders_count = Count('order')
+    # )
+    # create record
+    # collection = Collection(id=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
     result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
     context = {
         'name': 'Allen',
