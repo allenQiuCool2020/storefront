f53adf1b5db031048a7cc78c92280efd2cbf8083
Allen Qiu <118089569+allenQiuCool2020@users.noreply.github.com>
2024-04-10 15:33:23 +0800

    practicing-apr10
    practicing-apr10

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, Customer, Order, Collection
+from django.db.models import Value, F, Func, Count
+from django.db.models.functions import Concat
+
 # Create your views here.
 
 def hello_world(request):
-    queryset = Product.objects.filter(title__icontains='coffee')
+    # queryset = Customer.objects.annotate(
+    #     full_name = Concat(F('first_name'),Value(' ') ,F('last_name'))
+    # )
+
+    # grouping data
+
+    # queryset = Customer.objects.annotate(
+    #     orders_count = Count('order')
+    # )
+    # create record
+    # collection = Collection(id=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
+
+    # update specific one record
+    Collection.objects.filter(id=11).update(featured_product_id=2)
     context = {
         'name': 'Allen',
-        'products': list(queryset)
+        # 'result': list(queryset)
     }
     
     return render(request, 'hello.html',context)
